3ebf5e6f1d9c5da7463b6b93b2079ca62f2d8c27
Zack Youngren <zack@youngren.io>
2017-05-12 15:19:52 -0400

    Fix typo in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = ({lists, recipients}) => {
   const lists_string = lists === 1 ? 'List' : 'Lists';
-  const recipients_string = recipients === 1 ? ' Recipient' : 'Recipient';
+  const recipients_string = recipients === 1 ? ' Recipient' : 'Recipients';
 
   return (
     <div className="footer u-zeta u-borderTop u-paddingSidesHalf u-relative">
